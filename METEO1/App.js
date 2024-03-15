cca7277d482b211e90f31a707833e5736767e9c6
WissemMcheli <Wissemmcheli155@gmail.com>
2024-03-15 15:52:06 +0100

    bnjr

--- METEO1/App.js
+++ METEO1/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>slt les amis</Text>
+      <Text>bonjour</Text>
       <StatusBar style="auto" />
     </View>
   );
