85b4430ca2ecb87dbdf3a65b35a5fc83b4127e19
Ahmedkhalilchebbi <ahmedkhalilchebbi@gmail.com>
2024-03-28 16:44:31 +0200

    new Home div

--- METEO1/src/Screens/Home.js
+++ METEO1/src/Screens/Home.js
@@ -1,12 +1,13 @@
+
+
 import React, { useState } from 'react';
 import { View, ImageBackground, SafeAreaView, StyleSheet } from 'react-native';
-import Search  from '../Screens/Search';
+import TextInputWithSearch from './TextInputWithSearch';
 import LocationDisplay from './LocationDisplay';
 import WeatherDisplay from './WeatherDisplay';
 import ForecastDisplay from './ForecastDisplay';
-import One from '../../assets/ahmed.jpg'
-
-const API_KEY = 'd200832956edaca95a59aa25a27fe98b';
+import { getWeatherData, getForecastData, filterForecast } from '../fun/weatherFunctions';
+import One from '../../assets/ahmed.jpg';
 
 const Home = () => {
   const [city, setCity] = useState('');
@@ -17,35 +18,22 @@ const Home = () => {
     if (!city.trim()) return;
 
     try {
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-      const data = await response.json();
-      setWeather(data);
+      const weatherData = await getWeatherData(city);
+      setWeather(weatherData);
 
-      const forecastResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`);
-      const forecastData = await forecastResponse.json();
-      setForecast(filterForecast(forecastData.list));
+      const forecastData = await getForecastData(city);
+      const filteredForecast = filterForecast(forecastData);
+      setForecast(filteredForecast);
     } catch (error) {
-      alert("Erreur lors de la récupération des données météorologiques");
+      alert(error.message);
     }
-  };
-
-  const filterForecast = (forecastList) => {
-    const filteredForecast = {};
-    forecastList.forEach(item => {
-      const forecastDate = new Date(item.dt * 1000);
-      const dayOfWeek = forecastDate.toLocaleString('en-US', { weekday: 'long' });
-      if (!filteredForecast[dayOfWeek]) {
-        filteredForecast[dayOfWeek] = item;
-      }
-    });
-    return filteredForecast;
   };
 
   return (
     <ImageBackground source={One} style={styles.image}>
       <SafeAreaView style={{ flex: 1 }}>
-        <Search city={city} setCity={setCity} onPress={getWeather} />
-        {Object.keys(weather).length > 0 && Object.keys(forecast).length > 0 &&
+        <TextInputWithSearch city={city} setCity={setCity} onPress={getWeather} />
+        {Object.keys(weather).length > 0 && forecast.length > 0 &&
           <>
             <LocationDisplay name={weather?.name} country={weather?.sys?.country} />
             <WeatherDisplay temp={weather.main.temp} weather={weather.weather[0].main} />
